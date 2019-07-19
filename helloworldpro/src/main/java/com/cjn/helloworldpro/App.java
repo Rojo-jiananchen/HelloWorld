8a6b85782124d8999df91956cf4c5a355419aed9
jiananchen <jiananchen@10.15.184.39>
2019-07-19 13:36:56 +0200

    update

---
+++ helloworldpro/src/main/java/com/cjn/helloworldpro/App.java
@@ -0,0 +1,13 @@
+package com.cjn.helloworldpro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
